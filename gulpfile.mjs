e755fe1ac341680fb345b79ab0b6b646340ed9e6
SLundquist <113231921+SLundquist@users.noreply.github.com>
2024-10-10 14:55:06 +0200

    SCSS Variable Update
    Added and implemented variable for controlling transition time and curve for consistency.

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,7 +1,7 @@
 // gulpfile.mjs
 // Load Gulp and plugins using ESM syntax
 import gulp from 'gulp';
-import * as sass from 'sass';                                                   // Updated import syntax
+import * as sass from 'sass'; // Updated import syntax
 import gulpSass from 'gulp-sass';
 import autoprefixer from 'gulp-autoprefixer';
 import cleanCSS from 'gulp-clean-css';
@@ -13,35 +13,32 @@ const splitter = '--------------------------------------------------------------
 
 // Define file paths
 const paths = {
-  scss: './scss/**/*.scss',                                                     // SCSS pull location
-  css: './css'                                                                  // CSS output location
+  scss: './scss/**/*.scss', // SCSS pull location
+  css: './css', // CSS output location
 };
-
-// const separator = () => {
-//   log.console(splitter);
-// }
 
 // Compile SCSS to CSS
 const compileSCSS = () => {
-  console.log(splitter);                                                        // Insert separator variable
-  return gulp.src(paths.scss)
-    .pipe(sassCompiler().on('error', sassCompiler.logError))                    // Compile SCSS and handle errors
-    .pipe(autoprefixer())                                                       // Add vendor prefixes
-    .pipe(cleanCSS())                                                           // Minify CSS
-    .pipe(gulp.dest(paths.css))                                                 // Output CSS
-    .pipe(browserSyncServer.stream())                                           // Stream changes to browser   
+  console.log(splitter); // Insert separator variable
+  return gulp
+    .src(paths.scss)
+    .pipe(sassCompiler().on('error', sassCompiler.logError)) // Compile SCSS and handle errors
+    .pipe(autoprefixer()) // Add vendor prefixes
+    .pipe(cleanCSS()) // Minify CSS
+    .pipe(gulp.dest(paths.css)) // Output CSS
+    .pipe(browserSyncServer.stream()) // Stream changes to browser
     .on('end', () => console.log(splitter));
-  };
+};
 
 // Watch SCSS files for changes
 const watchFiles = () => {
   browserSyncServer.init({
     server: {
-      baseDir: './'                                                             // Base directory for BrowserSync
-    }
+      baseDir: './', // Base directory for BrowserSync
+    },
   });
   gulp.watch(paths.scss, compileSCSS); // Watch SCSS files
-  gulp.watch('./*.html').on('change', browserSyncServer.reload);                // Reload on HTML changes
+  gulp.watch('./*.html').on('change', browserSyncServer.reload); // Reload on HTML changes
 };
 
 // Export tasks
